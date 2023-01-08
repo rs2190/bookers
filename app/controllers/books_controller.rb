f4ac8aead062a44752c7a3d80b813b5444d4eeb3
rs2190 <122139610+rs2190@users.noreply.github.com>
2023-01-08 09:56:56 +0000

    [Add] 2023/01/08 新規追加用のテキストエリア追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,8 @@
 class BooksController < ApplicationController
   def new
+
+    @book = book.new
+
   end
 
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
 
   get 'homes/top'
 
-  get 'books/new'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
+  resources :books
+
+  # get 'books' => 'books#new'
+  # get 'books/index'
+  # get 'books/show'
+  # get 'books/edit'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
